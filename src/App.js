a1ce33c126a9e7ce012a8891bd54bab0e62343cb
Gankhulug02 <huluguu0202@gmail.com>
2023-03-30 17:52:34 +0800

    asd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
+  const [branches, setBranches] = useState([]);
+
+  const getAllShop = async () => {
+    try {
+      const res = await axios.get("http://localhost:8010/restaurants");
+      setBranches(res.data.restaurants);
+      console.log(res.data.restaurants);
+    } catch (error) {}
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>газрын зураг</h1>
+      <div>
+        <button onClick={getAllShop}>All coffe shop</button>
+        <button>ugugdsun zaid oiriig haruulah</button>
+      </div>
+      <div style={{ width: "100%", height: "90vh" }}>
+        map
+        <MapContainer
+          style={{ width: "100%", height: "90vh" }}
+          center={[47.923742823223996, 106.93408876169889]}
+          zoom={17}
+          scrollWheelZoom={true}
         >
-          Learn React
-        </a>
-      </header>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+
+          {branches.length > 0 &&
+            branches.map((r, index) => (
+              <Marker
+                key={index}
+                position={[
+                  r.location.coordinates[1],
+                  r.location.coordinates[0],
+                ]}
+              >
+                <Popup>
+                  A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+              </Marker>
+            ))}
+        </MapContainer>
+      </div>
     </div>
   );
 }
